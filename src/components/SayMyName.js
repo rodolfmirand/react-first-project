5da68ec5a9862ef706980c1631ded46849b77c03
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-03-10 11:25:47 -0300

    create components

---
+++ src/components/SayMyName.js
@@ -0,0 +1,14 @@
+function SayMyName(props) {
+    return (
+        <div>
+            <p>
+                Now... Say my name.
+                <br></br>
+                {props.name}.
+                <br></br>
+                You're goddamn  right.
+            </p>
+        </div>
+    )
+}
+export default SayMyName
