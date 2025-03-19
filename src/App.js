c7c7f4b9b0b9834b0a0ffc424d3bf1c4b89bbf6a
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-03-19 10:15:38 -0300

    modify app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
-import List from './components/List';
 import Person from './components/person/Person';
 import SayMyName from './components/sayMyName/SayMyName';
+import Event from './components/event/Event';
+import Form from './components/form/Form'
 
 function App() {
   const name = 'Walter White'
 
   return (
     <div id='container'>
+      <Person name={name} age={52} occupation="Drug Dealer" image="/mr-white.jpeg" />
       <SayMyName name="Heisenberg" />
-      <Person name={name} age="52" occupation="Drug Dealer" image="/mr-white.jpeg" />
-      <List />
+
+      <Event number={1} />
+      <Form />
     </div>
   );
 }
