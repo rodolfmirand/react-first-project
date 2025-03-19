8cf903d2156b6f8a2ed560e4296f0d4772327836
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-03-19 10:14:05 -0300

    create form

---
+++ src/components/form/Form.js
@@ -0,0 +1,44 @@
+import styles from './Form.module.css'
+import { useState } from 'react'
+
+function Form() {
+
+    function registerUser(e) {
+        e.preventDefault()
+        console.log(`User registred! Name: ${name} | Password: ${password}`)
+    }
+
+    const [name, setName] = useState('Default')
+    const [password, setPassword] = useState()
+
+    return (
+        <div className={styles.formContainer}>
+            <h1>Register</h1>
+            <form onSubmit={registerUser}>
+                <div className={styles.formGroup}>
+                    <label htmlFor="name">Name: </label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div className={styles.formGroup}>
+                    <label htmlFor="password">Password: </label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div>
+                    <input type="submit" value="Register" />
+                </div>
+            </form>
+        </div>
+    )
+}
+export default Form
